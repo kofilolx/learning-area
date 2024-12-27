2da2108adba52bc32ea350c94f0fde0379860933
kofilolx <kofyamando@gmail.com>
2024-12-27 18:05:33 +0000

    register validationfixed

--- cloneClub/django_project/django_project/urls.py
+++ cloneClub/django_project/django_project/urls.py
@@ -16,10 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from users import views as user_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')), #trailing slash for navigation, redirection of route
-    #path('', include('users.urls')),
-
+    path('register/', user_views.register, name='register'),
+    path('login/', user_views.login, name='login')
 ]
--- cloneClub/django_project/users/views.py
+++ cloneClub/django_project/users/views.py
@@ -1,6 +1,28 @@
-from django.shortcuts import render
-from django.contrib.auth.forms import UserCreaationForm
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 
 def register(request):
-    form = UserCreaationForm()
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            form.save()  # Save the new user to the database
+            messages.success(request, f'Account created for {username}!')
+            return redirect('home')
+    else:
+        form = UserCreationForm()  # Instantiate a new form for GET requests
     return render(request, 'users/register.html', {'form': form})
+
+def login(request):
+
+    if request.method =='POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get(username)
+            form.save()
+            messages.success(request, f'Login successful {username}')
+            return reidercct('home')
+    else:
+        form = UserCreationForm()
+    return render(request, 'users/login.html', {'form':form})
